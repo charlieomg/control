d323fe64392a5fd55783a4212ac0b4ff1c2e3195
charlieomg <143298651+charlieomg@users.noreply.github.com>
2026-08-20 00:32:27 -0700

    Change message displayed in javascripttoinject.js

--- javascripttoinject.js
+++ javascripttoinject.js
@@ -1,2 +1,2 @@
 
-document.documentElement.innerHTML = "<body style='background:#000;color:#ff0055;font-family:monospace;display:flex;justify-content:center;align-items:center;height:100vh;margin:0'><div style='font-size:48px;font-weight:bold;text-shadow:0 0 20px #ff0055'>get trolled lol</div></body>";
+document.documentElement.innerHTML = "<body style='background:#000;color:#ff0055;font-family:monospace;display:flex;justify-content:center;align-items:center;height:100vh;margin:0'><div style='font-size:48px;font-weight:bold;text-shadow:0 0 20px #ff0055'>I CONTROL EVERYTHING</div></body>";
